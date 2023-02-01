dcd572c24808f851fe8aef692ce1ea8e05470ec1
Martin <fridaymartin500@gmail.com>
2023-02-01 17:24:44 +0100

    Updated the page to check user requests before showing success message

--- app.js
+++ app.js
@@ -12,49 +12,54 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  
+
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const email = req.body.email;
 
-  const data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName,
-        }
-      }
-    ]
-  };
-
-  const jsonData = JSON.stringify(data);
-
-  const url = "https://us17.api.mailchimp.com/3.0/lists/bab2c3439b";
-  const options = {
-    method: "POST",
-    auth: "Martinelli99:0a55624aa5e8e664b78497964a1cebea-us17"
+  if (firstName === "" || lastName === "" || email === "") {
+    res.sendFile(__dirname + "/failure.html");
   }
+  else {
+    const data = {
+      members: [
+        {
+          email_address: email,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName,
+          }
+        }
+      ]
+    };
 
-  const request = https.request(url, options, function(response) {
+    const jsonData = JSON.stringify(data);
 
-    if (response.statusCode === 200) {
-      res.sendFile(__dirname + "/success.html");
-    } else {
-      res.sendFile(__dirname + "/failure.html");
+    const url = "https://us17.api.mailchimp.com/3.0/lists/bab2c3439b";
+    const options = {
+      method: "POST",
+      auth: "Martinelli99:0a55624aa5e8e664b78497964a1cebea-us17"
     }
 
-    response.on("data", function(data){
-      console.log(JSON.parse(data));
+    const request = https.request(url, options, function(response) {
+
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+
+      response.on("data", function(data){
+        console.log(JSON.parse(data));
+      });
     });
-  });
 
-  request.write(jsonData);
-  request.end();
-
+    request.write(jsonData);
+    request.end();
+  }
 });
+
 
 app.post("/failure", function(req, res) {
   res.redirect("/");
